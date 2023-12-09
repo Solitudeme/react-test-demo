331757ffa1380591e4e124bf1eaff8c110132e89
zh-lx <18366276315@163.com>
2023-12-09 12:59:49 +0800

    feat: update page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import './App.css'
+
+interface TeacherType {
+  name: string;
+  students: string[]
+}
 
 function App() {
   const [teacherName, setTeacherName] = useState('')
   const [studentName, setStudentName] = useState('')
+  const [teachers, setTeachers] = useState<TeacherType[]>()
 
-  function handleTeacherNameChange(e: any) {
+  function handleTeacherNameChange(e: ChangeEvent<HTMLInputElement>) {
     setTeacherName(e.target.value)
   }
 
-  function handleStudentNameChange(e: any) {
+  function handleStudentNameChange(e: ChangeEvent<HTMLInputElement>) {
     setStudentName(e.target.value)
   }
+
+  function handleAddTeacher() {
+
+  }
+
+  function handleRemoveTeacher() {
+
+  }
+
+  function handleAddStudent() {
+
+  }
+
   return (
     <>
       <div>
@@ -23,10 +42,12 @@ function App() {
         <input value={studentName} onChange={handleStudentNameChange} />
       </div >
       <div className='operations'>
-        <button>招聘教师</button>
-        <button>解雇教师</button>
-        <button>招收学生</button>
-        <button>开除学生</button>
+        <button onClick={handleAddTeacher}>招聘教师</button>
+        <button onClick={handleRemoveTeacher}>解雇教师</button>
+        <button onClick={handleAddStudent}>招收学生</button>
+      </div>
+      <div className="teachers">
+        {/* 教师学生列表区 */}
       </div>
     </>
   )
